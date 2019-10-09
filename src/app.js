a7061953321666bd3b9feae3654a18355ba111c7
Patrick Schadler <patrick.schadler@gmail.com>
2019-10-09 13:48:45 +0000

    feat(grocery-list): move set state into component did mount.

---
+++ src/app.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import {
+  Box,
+  Button,
+  Collapsible,
+  Heading,
+  Grommet,
+  ResponsiveContext
+} from 'grommet';
+import { Notification } from 'grommet-icons';
+import { Incident } from './incident';
+import './app.css';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px'
+    }
+  }
+};
+
+const AppBar = props => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation="medium"
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
+class App extends Component {
+    state = {
+        showSidebar: false,
+        incidents: []
+    };
+
+    componentDidMount(){
+        let incidents = [];
+
+        for (let i = 1; i <= 10; i++) {
+        incidents.push({
+            title: 'Incident ' + i,
+            content: 'Our server responded with an internal error'
+        });
+        }
+
+        this.setState({incidents: incidents});
+    }
+
+    render() {
+        const { showSidebar } = this.state;
+
+        const incidents = this.state.incidents.map((item, key) => (
+        <Incident title={item.title} content={item.content}></Incident>
+        ));
+
+        return (
+        <Grommet theme={theme} full>
+            <ResponsiveContext.Consumer>
+            {size => (
+                <Box fill>
+                <AppBar>
+                    <Heading level="3" margin="none">
+                    Statusly
+                    </Heading>
+                    <Button
+                    icon={<Notification />}
+                    onClick={() =>
+                        this.setState(prevState => ({
+                        showSidebar: !prevState.showSidebar
+                        }))
+                    }
+                    />
+                </AppBar>
+                <Box
+                    className="block"
+                    direction="row"
+                    flex
+                    overflow={{ horizontal: 'hidden' }}
+                >
+                    <Box className="content" flex align="center" justify="center">
+                    {incidents}
+                    </Box>
+                    {size !== 'small' && (
+                    <Collapsible direction="horizontal" open={showSidebar}>
+                        <Box
+                        flex
+                        width="medium"
+                        background="light-2"
+                        elevation="small"
+                        align="center"
+                        justify="center"
+                        >
+                        sidebar
+                        </Box>
+                    </Collapsible>
+                    )}
+                </Box>
+                </Box>
+            )}
+            </ResponsiveContext.Consumer>
+        </Grommet>
+    );
+  }
+}
+
+export default App;
